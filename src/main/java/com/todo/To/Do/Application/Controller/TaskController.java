e1bdcfefb0d81f8b5f647e75ac023a7fc0a735f0
MahalakshmiKasthurirengan <mahalakshmikasthurirengan22@gmail.com>
2025-05-27 22:05:47 +0530

    Created Crud operations

--- src/main/java/com/todo/To/Do/Application/Controller/TaskController.java
+++ src/main/java/com/todo/To/Do/Application/Controller/TaskController.java
@@ -5,8 +5,7 @@ import org.springframework.ui.Model;
 import com.todo.To.Do.Application.Model.Task;
 import com.todo.To.Do.Application.Service.TaskService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +21,23 @@ public class TaskController {
         List<Task> tasks = taskService.getAllTasks();
         model.addAttribute("tasks", tasks);
         return "tasks";
+    }
+
+    @PostMapping
+    public String createTasks(@RequestParam String title) {
+        taskService.createTask(title);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/toggle")
+    public String toggleTask(@PathVariable Long id) {
+        taskService.toggleTask(id);
+        return "redirect:/";
     }
 }
